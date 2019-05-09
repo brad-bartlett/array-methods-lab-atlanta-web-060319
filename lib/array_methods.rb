56094c839d73fc6b839010aa4e3d55a337a02004
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-09 22:53:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,11 +4,11 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
@@ -16,9 +16,9 @@ def using_first(array)
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
-
+  array.size
 end
