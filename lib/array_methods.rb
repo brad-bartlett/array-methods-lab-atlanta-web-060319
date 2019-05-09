71d8486eb007fdd24e5e7c656db5ac0ea2ead5ad
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-09 22:51:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,10 @@
 def using_include(array, element)
-  famous_cats = ["wow", "Bob", "Russ"]
-  famous_cats.include?("Ada")
+  famous_cats.include?
 
 end
 
 def using_sort(array)
-  fam
+  
 end
 
 def using_reverse(array)
@@ -13,8 +12,7 @@ def using_reverse(array)
 end
 
 def using_first(array)
-  famous_cats = ["wow", "Bob", "Russ"]
-  famous_cats.first
+  array.first
 end
 
 def using_last(array)
